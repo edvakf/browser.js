a98ef8f61b50e0056e9e6593a38e941dfd064737
Atsushit Takayama <taka.atsushi@gmail.com>
2009-09-23 03:20:00 -0500

    9.60 / Wed Sep 23 08:20:00 UTC 2009

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-// vwgXUHjg6/nzXdxEStJHnRPfcm/V+XH5twDPcOX2LG4pGSit6OZvOOI++9Sc9qhUuMuI8l0qaqC1OrIRhJ3GWbB0Np1bUb/AG7HaCIfpQECNcOdnQnkstqD/j2G9I39+H3Tj4bv3PSg8JsgWlq4piX783H32SO94dBAJUKxkvIRtfF97C1bKN3JnkASLC6T2/CzQjOET62woIpfW5qi2/GQopMMBrDbM0CvvxHIixGsnGk40FF+uOAl4DBmxgXXKSjjBopOgckpp0SRFGGgm/RvrcCW/PreRfdvy60O5fEKy+AqCOmecuttPTakMiKB/b+prWYcCPbxyA8rvsyH1Kw==
+// cRedhPlV/tHliQSYLW1s5RCwrZgweeQSyLfrG/AeRIhsoHZlmwOrp2Vqu5bCQrVb/vGK42rIfcaZu01bhm5lLtmvs1Sp68JThBKTYEkxkbxWZnTNzyVfGeO1HHIa/4X/FCKyeoLw/EP0hEYqHV2H29yn8MJcdbZGUY/EPzzKRzBpQtZwqnWMzucmMWpqUtwO7iWkXY6shjvGK2xrFehCnuYuO6OPbOtXjS54kvAfzyoMb15QJFy44D2Umj15PM8Myl7r7szWGx2dEyVn2X7tt6O27HJ8BOnCnAzd1+kT6Yt47upgG3Ib7ezItyq4BqCEbTbqgYumKSXkp5QL7AmB0A==
 /**
 ** Copyright (C) 2000-2009 Opera Software AS.  All rights reserved.
 **
@@ -16,7 +16,7 @@
 **/
 // Generic fixes (mostly)
 (function(opera){
-	var bjsversion=' Opera  9.60, Desktop, September 15, 2009 ';
+	var bjsversion=' Opera  9.60, Desktop, September 22, 2009 ';
 	// variables and utility functions
 	var navRestore = {}; // keep original navigator.* values
 	var shouldRestore = false;
@@ -1356,6 +1356,14 @@ function solveEventOrderBugs(){
 			addPreprocessHandler( /d\.location\.href = ".*?\/browser_upgrade\.html";/g, '');
 				if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Yahoo ISP portal blocks Opera users). See browser.js for details');
 		}
+		if(hostname.indexOf('mail.yahoo')>-1){			// DSK-263826, Keyboard navigation of autocomplete menu fails
+			opera.addEventListener('BeforeEvent.keypress', function(e){
+				if( e.event.keyCode>36 && e.event.keyCode<41 ){
+					e.event.charCode=0;
+				}
+			}, false);
+				if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Keyboard navigation of autocomplete menu fails). See browser.js for details');
+		}
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Yahoo!). See browser.js for details');
 	} else if(hostname.indexOf('allabout.co.jp')!=-1){			// DSK-227082, Works around script scheduling bug on AllAbout
 		fixJQueryScriptSchedulingTrouble();
@@ -1375,6 +1383,19 @@ function solveEventOrderBugs(){
 	} else if(hostname.indexOf('apple.viamichelin.com')>-1){			// 288490, Text on Apple store locator page is misaligned and overlapping
 		addCssToDocument('center table{text-align: left} div#poilist table td img+img{ display: block;}')
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Text on Apple store locator page is misaligned and overlapping). See browser.js for details');
+	} else if(hostname.indexOf('athome.co.jp') > -1){			// PATCH-147, athome.co.jp Animated menu doesn't appear
+		opera.defineMagicFunction('showMenu',function(oRealFunc,oThis,menu,tab,scroll){
+			try{var tgt=$("SELECT_TAB_" + menu);
+			if (tgt&&(tgt.innerHTML==unescape('%0D%0A'))) { tgt.innerHTML=unescape('%0A'); }}catch(e){}
+			oRealFunc.apply(oThis,arguments.slice(2));
+		},false);
+		
+				// PATCH-147, athome.co.jp Hide warning messages because of Browser UA
+		opera.defineMagicFunction('checkTargetBrowser',function(){});
+		opera.defineMagicFunction('checkTargetCookie',function(){});
+				// PATCH-147, athome.co.jp font-size fix
+		addCssToDocument('body {font-size:x-small !important;}');
+			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (athome.co.jp Animated menu doesn\'t appear\nathome.co.jp Hide warning messages because of Browser UA\...). See browser.js for details');
 	} else if(hostname.indexOf('att.com')!=-1){			// PATCH-36, ATT / Bellsouth browser sniffing
 		opera.defineMagicVariable( 'isDHTML', function(){return true;}, null );
 		opera.defineMagicFunction('checkBrowser', function(){});
@@ -1584,9 +1605,6 @@ function solveEventOrderBugs(){
 	} else if(hostname.indexOf('kr.msn.com')!=-1){			// 349584, head layout broken on kr.msn.com
 		addCssToDocument('li:after, ul:after{display:none!important}');
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (head layout broken on kr.msn.com). See browser.js for details');
-	} else if(hostname.indexOf('kubuntu.org')!=-1){			// PATCH-47, Nested z-index hides content
-		addCssToDocument('.sb, .sbi, .sb *, .sbi * {z-index:auto !important}');
-			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Nested z-index hides content). See browser.js for details');
 	} else if(hostname.indexOf('lgmobile.com')!=-1 ){			// 331748,  LG Mobile Flash does not load as expected because of missing type attribute
 		document.addEventListener( 'DOMContentLoaded', function(e){
 			for( var elements=document.getElementsByTagName('object'),element,i=0; element=elements[i]; i++){
