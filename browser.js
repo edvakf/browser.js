d933579d895eb270a003868ba2572ff2b4f05fb3
Atsushit Takayama <taka.atsushi@gmail.com>
2011-03-23 05:03:00 -0500

    11.10 / Wed Mar 23 10:03:00 UTC 2011

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-// utCzAWEj2aDbEjz4ngrKXNu9ZFRCqyAqurObPIjDnWKkWKhLqUcvwrm51ojT+MJyJw7zOi2/IF6+tGy0iI0SMnLNalZFL3BLsCKlBdfwryNkzNzILwJ7TGUY9ZZ6FZwim9x/m5es14J9mymqvhID4sMBVfykeIAYekijsV18X+kM0/Zx6Kr8DOdivVAzEpkzruaUTWc2WD3/VjZiRhcvLm4rMxqlZ5IvFANsf2U+q30d1e9tZeXjsb1s/PuxDQ2wJyVgzKhgMAaQ9CUzQfta/597LdKXaKmjks2gw8Yo0MySTlDeZ1tNGDOfpE9s409NOugE/pQga6dqhAbTqsvcqA==
+// clOxIkZtPUuIQ+QsPI9YDKDRsWLpazl/4No4DejEH6oD7OKw9XhF8xCC6yxq/kDefgx529nmkpoMCsC1YVTmZOlEIrTM751wIcA/xb0mMiC31+4csp861zS61yHiQ6TZtRhuPG5O+RdYXtJv7VJVUxALSriwUglS2wdvGSl9oHDwKOiJLxBJi/lEwPovZ6SGId4I/1Rp/8D2XJQDRjMeJ+Klgf9HnBt9gUOEr0LHD6Kh8vK5xcnjesoei96G7a2qomW3yLNUpONukD/nmomG0ZOtaXpEhkZGwH+9WuhdbAkHu/o8bvNiSDqceTW2Zrim/QoCILsksB5Z+r2USdIb+Q==
 /**
 ** Copyright (C) 2000-2011 Opera Software AS.  All rights reserved.
 **
@@ -18,7 +18,7 @@
 (function(opera){
 	if(!opera || (opera&&opera._browserjsran))return;
 	opera._browserjsran=true;
-	var bjsversion=' Opera Desktop 11.10 core 2.8.99, March 10, 2011 ';
+	var bjsversion=' Opera Desktop 11.10 core 2.8.119, March 22, 2011 ';
 	// variables and utility functions
 	var navRestore = {}; // keep original navigator.* values
 	var shouldRestore = false;
@@ -553,6 +553,15 @@ function setTinyMCEVersion(e){
 				}else{
 					window.attachEvent=undefined; 
 					addEventListener.call(opera, 'AfterScript', function(e){
+						if( window.FB && FB.init ){
+							(function(init){
+								FB.init=function(){
+									window.attachEvent=undefined;
+									init.apply(this,arguments);
+									window.attachEvent=win_attachEvent;
+								}
+							})(FB.init);
+						}
 						window.attachEvent=win_attachEvent;
 						removeEventListener.call(opera, 'AfterScript', arguments.callee, false);
 					}, false);
@@ -702,6 +711,17 @@ function setTinyMCEVersion(e){
 				if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Converting RGB to Hex confuses "high contrast mode" detection). See browser.js for details');
 		}
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (AOL). See browser.js for details');
+	} else if(hostname.indexOf('.apple.com')>-1){			// PATCH-385, Apple.com thinks Opera's CSS property vendor prefix is o instead of O on JS properties
+		(function (){
+		for(var ar=['TransitionProperty', 'TransitionDuration', 'TransitionTimingFunction' ],i=0;i<ar.length; i++){
+			CSSStyleDeclaration.prototype.__defineSetter__('o'+ar[i], (function(prop){return function(val){ 
+				this['O'+prop]=val; 
+			}})(ar[i]));
+		}
+		})();
+				// PATCH-387, Make Apple menu visible
+		addCssToDocument('#globalnav li { width: auto !important}');
+			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Apple.com thinks Opera\'s CSS property vendor prefix is o instead of O on JS properties\nMake Apple m...). See browser.js for details');
 	} else if(hostname.indexOf('.dell.')!=-1&&hostname.indexOf('support.')!=-1){			// 286618,  browser sniffing on support.dell.com
 		opera.defineMagicVariable( 'ig_shared', null, function(o){ o.IsNetscape6=true; return o; } );
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' ( browser sniffing on support.dell.com). See browser.js for details');
@@ -1077,6 +1097,19 @@ function setTinyMCEVersion(e){
 		}, false );
 		
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Caja Madrid hides login form by CSS mistake). See browser.js for details');
+	} else if(hostname.indexOf('calendar.live.com')>-1){			// PATCH-389, Events are shown in squished box because table cells are containers for positioned descendants
+		addCssToDocument('.monthGrid td.eventContainerRow{position: static ! important;}');
+				// PATCH-389, Calendar does not respond to mouse due to event.button being W3C-compatible
+		function fixButton(e) {
+			if (e.button == 1) {
+				e.__defineGetter__('button', function() { return 0 });
+			}
+		};
+		window.addEventListener('mousedown', fixButton, true);
+		window.addEventListener('mousemove', fixButton, true);
+		window.addEventListener('mouseup', fixButton, true);
+		
+			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Events are shown in squished box because table cells are containers for positioned descendants\nCale...). See browser.js for details');
 	} else if(hostname.indexOf('cambrian.mb.ca')>-1){			// PATCH-285, Enable log-in button on Cambrian bank
 		Element.prototype.__defineGetter__('all', function(){});
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Enable log-in button on Cambrian bank). See browser.js for details');
@@ -1175,6 +1208,14 @@ function setTinyMCEVersion(e){
 	} else if(hostname.indexOf('fujifilm.ch')>-1){			// PATCH-220, Working around a bug that hides menu entries
 		addCssToDocument('#navigation ul#primary li ul.secondary_drop_down li {display: inline !important }');
 			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (Working around a bug that hides menu entries). See browser.js for details');
+	} else if(hostname.indexOf('g4tv.com')>-1){			// PATCH-281, messed up layout in lower part of page due to wrong percentage rounding
+		window.addEventListener('DOMContentLoaded', function(e){
+			var el;
+			if(el=document.getElementsByClassName('content-list editorial variation-2')[0]){
+				el.style.width='634px';
+			}
+		}, false);
+			if(self==top)postError.call(opera, 'Opera has modified the JavaScript on '+hostname+' (messed up layout in lower part of page due to wrong percentage rounding). See browser.js for details');
 	} else if(hostname.indexOf('geoaccess.com')!=-1){			// 318050,  BlueCross browser sniffing prevents insurance search
 		opera.defineMagicVariable('is_nav', function(){return true;}, null);
 		
